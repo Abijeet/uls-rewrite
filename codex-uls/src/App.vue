bf0a615dc04d14de9b85c2a0917e330425349c59
Abijeet <abijeetpatro@gmail.com>
2025-10-15 22:21:38 +0530

    codex-uls: Remove unnecessary code from App.vue

--- codex-uls/src/App.vue
+++ codex-uls/src/App.vue
@@ -1,9 +1,4 @@
 <template>
-	<div>
-		<cdx-button action="destructive">
-			<cdx-icon :icon="cdxIconTrash" /> Delete this item
-		</cdx-button>
-	</div>
 	<hr>
 	<div class="language-selector-wrapper">
 		<label>Select a language:</label>
@@ -19,22 +14,15 @@
 
 <script>
 import { defineComponent } from 'vue';
-import { CdxButton, CdxIcon } from '@wikimedia/codex';
-import { cdxIconTrash } from '@wikimedia/codex-icons';
 import LanguageSelector from './components/LanguageSelector.vue';
 import MultipleLanguageSelector from './components/MultipleLanguageSelector.vue';
 
 export default defineComponent( {
 	components: {
-		CdxButton,
-		CdxIcon,
 		LanguageSelector,
 		MultipleLanguageSelector
 	},
     setup() {
-        return {
-            cdxIconTrash
-        }
     }
 } );
 </script>
