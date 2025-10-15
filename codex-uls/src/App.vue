44398bd682b21ca3298beb7d319b4dd482ea7197
Abijeet <abijeetpatro@gmail.com>
2025-10-15 17:17:38 +0530

    codex-uls: Base setup

---
+++ codex-uls/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+	<div>
+		<cdx-button action="destructive">
+			<cdx-icon :icon="cdxIconTrash" /> Delete this item
+		</cdx-button>
+	</div>
+	<hr>
+	<div class="language-selector-wrapper">
+		<label>Select a language:</label>
+		<language-selector :is-multiselect="false" />
+	</div>
+	<div class="language-selector-wrapper">
+		<div >
+		<label>Select multiple languages:</label>
+		<language-selector :is-multiselect="true" />
+	</div>
+	</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { CdxButton, CdxIcon } from '@wikimedia/codex';
+import { cdxIconTrash } from '@wikimedia/codex-icons';
+import LanguageSelector from './components/LanguageSelector/LanguageSelector.vue';
+
+export default defineComponent( {
+	components: {
+		CdxButton,
+		CdxIcon,
+		LanguageSelector
+	},
+    setup() {
+        return {
+            cdxIconTrash
+        }
+    }
+} );
+</script>
+
+<style>
+@import '@wikimedia/codex/dist/codex.style.css';
+
+.language-selector-wrapper {
+	width: 300px;
+	margin-bottom: 1em;
+	label {
+		margin-bottom: 0.5em;
+		display: block
+	}
+}
+</style>
---
+++ codex-uls/src/components/LanguageSelector/LanguageSelector.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+		<cdx-lookup
+            v-if="!isMultiselect"
+            v-model:selected="selectedLanguage"
+            :menu-items="languages"
+            placeholder="Select a language"
+        />
+        <cdx-multiselect-lookup
+            v-else
+            v-model:selected="selectedLanguages"
+            v-model:input-chips="selectedLanguageChips"
+            :menu-items="languages"
+            placeholder="Select languages"
+        />
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import { CdxLookup, CdxMultiselectLookup } from '@wikimedia/codex';
+
+export default {
+    components: { CdxLookup, CdxMultiselectLookup },
+    props: {
+        isMultiselect: {
+            type: Boolean,
+            default: false
+        }
+    },
+    setup(props) {
+        const languages = [
+            { label: 'English', value: 'en' },
+            { label: 'Spanish', value: 'es' },
+            { label: 'French', value: 'fr' },
+            { label: 'German', value: 'de' },
+            { label: 'Chinese', value: 'zh' }
+        ];
+
+        const selectedLanguage = ref(null);
+        const selectedLanguages = ref([]);
+        const selectedLanguageChips = ref([]);
+
+        return {
+            languages,
+            isMultiselect: props.isMultiselect,
+            selectedLanguage,
+            selectedLanguages,
+            selectedLanguageChips
+        };
+    }
+};
+</script>
