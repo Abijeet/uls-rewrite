dd20740a55a7761f34603d88d5119a3c1c277632
Abijeet <abijeetpatro@gmail.com>
2025-10-16 13:57:52 +0530

    codex-uls: Use CdxField, CdxInpu to wrap around language selectors

--- codex-uls/src/App.vue
+++ codex-uls/src/App.vue
@@ -1,37 +1,59 @@
 <template>
 	<hr>
 	<div class="language-selector-wrapper">
-		<label>Select a language:</label>
-		<language-selector placeholder="Select a language">
-			<template #no-results="{ inputValue }">
-				<p>The page is not available in the selected language, <strong>{{ inputValue }}</strong></p>
-				<p>It maybe available in other languages:</p>
-				<ol>
-					<li>English</li>
-					<li>Hindi</li>
-				</ol>
+		<cdx-field>
+			<language-selector placeholder="Select a language">
+				<template #no-results="{ inputValue }">
+					<p>The page is not available in the selected language, <strong>{{ inputValue }}</strong></p>
+					<p>It maybe available in other languages:</p>
+					<ol>
+						<li>English</li>
+						<li>Hindi</li>
+					</ol>
+				</template>
+			</language-selector>
+			<template #label>
+				Select a language
 			</template>
-		</language-selector>
+		</cdx-field>
+		
+		<cdx-field>
+			<cdx-text-input v-model="highlightedLanguages" />
+			<template #label>
+				Languages to highlight
+			</template>
+			<template #description>
+				Enter comma separated list of languages codes to highlight. e.g. en,hi
+			</template>
+		</cdx-field>
 	</div>
+	<hr>
 	<div class="language-selector-wrapper">
-		<div >
-		<label>Select multiple languages:</label>
-		<multiple-language-selector placeholder="Select languages" />
-	</div>
+		<cdx-field>
+			<multiple-language-selector placeholder="Select languages" />
+			<template #label>
+				Select multiple languages
+			</template>
+		</cdx-field>
 	</div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import { CdxField, CdxTextInput } from '@wikimedia/codex';
 import LanguageSelector from './components/LanguageSelector.vue';
 import MultipleLanguageSelector from './components/MultipleLanguageSelector.vue';
 
 export default defineComponent( {
 	components: {
 		LanguageSelector,
-		MultipleLanguageSelector
+		MultipleLanguageSelector,
+		CdxTextInput,
+		CdxField
 	},
     setup() {
+		const highlightedLanguages = ref( 'en,hi' );
+		return { highlightedLanguages };
     }
 } );
 </script>
@@ -40,7 +62,7 @@ export default defineComponent( {
 @import '@wikimedia/codex/dist/codex.style.css';
 
 .language-selector-wrapper {
-	width: 300px;
+	width: 450px;
 	margin-bottom: 1em;
 	label {
 		margin-bottom: 0.5em;
